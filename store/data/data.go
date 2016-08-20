174252b1b114242f1a749c22fde76a364b0e1e9c
Thomas Boerger <thomas@webhippie.de>
2016-08-20 17:11:19 +0200

    Added admin flag to user

--- store/data/data.go
+++ store/data/data.go
@@ -248,6 +248,7 @@ func migrateDatabase(driver string, db *gorm.DB) error {
 			Password: "admin",
 			Email:    "admin@example.com",
 			Active:   true,
+			Admin:    true,
 		}
 
 		err := db.Create(
