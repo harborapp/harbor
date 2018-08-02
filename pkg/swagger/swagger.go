27e6d701e78480d840b583c4aabf12519cc32637
Thomas Boerger <thomas@webhippie.de>
2018-08-02 16:45:32 +0200

    Integrate swagger definition

--- pkg/swagger/swagger.go
+++ pkg/swagger/swagger.go
@@ -1,3 +1,9 @@
 package swagger
 
-//go:generate retool -tool-dir ../../_tools do swagger generate spec -o ../../assets/swagger.json
+import (
+	// Dummy import to get it known to go dep.
+	_ "golang.org/x/net/webdav"
+)
+
+//go:generate swagger generate spec -o swagger.json
+//go:generate fileb0x ab0x.yaml
